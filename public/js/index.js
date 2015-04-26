7447c027dbccbd1438c43a26211ffc0dd9d59d7c
Xavier Tristancho <xavi@letitguide.com>
2015-04-26 17:23:43 +0200

    Updated Project Dependencies

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,27 @@
 var angular = require('angular');
 
 require('angular-resource');
+require('angular-animate');
 require('angular-ui-router');
 require('angular-filter');
-require('sweetalert');
-require('angular-sweetalert');
+require('angular-translate');
 
 var ngModule = angular.module('app', [
+	'ngAnimate',
     'ngResource',
     'ui.router',
     'angular.filter',
-    'oitozero.ngSweetAlert'
+    'oitozero.ngSweetAlert',
+    'toaster',
+    'pascalprecht.translate'
 ]);
 
 require('./config')(ngModule);
+require('./locale')(ngModule);
+require('./api')(ngModule);
+require('./alerts')(ngModule);
+require('./notifications')(ngModule);
+require('./directives')(ngModule);
 require('./filters')(ngModule);
 require('./projects')(ngModule);
 require('./projects/repositories')(ngModule);
