b6fa83d09e3f999206a733231445a677eae93a10
Xavier Tristancho <xavi@letitguide.com>
2015-04-26 17:22:16 +0200

    Deleted Comments

--- public/js/config/routes.js
+++ public/js/config/routes.js
@@ -1,11 +1,9 @@
 module.exports = function(ngModule)
 {
     ngModule.config(function($stateProvider, $urlRouterProvider) {
-        //
-        // For any unmatched url, redirect to /state1
+
         $urlRouterProvider.otherwise("/");
-        //
-        // Now set up the states
+
         $stateProvider
             .state('home', {
                 url: "/",
