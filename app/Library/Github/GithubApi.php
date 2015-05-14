8907f128342ec32b9137b702a333cf52be5d02e0
Xavier Tristancho <xavi@letitguide.com>
2015-05-14 23:01:13 +0200

    Refactored GithubApi

--- app/Library/Github/GithubApi.php
+++ app/Library/Github/GithubApi.php
@@ -9,34 +9,59 @@ class GithubApi {
 
     public static function getDependencyName($fields)
     {
+        $response = null;
         $manager = Manager::find($fields['manager']);
         $name = explode('/', $fields['name']);
-        $url = "http://raw.githubusercontent.com/{$name[0]}/{$name[1]}/master/";
 
-        $client = new Client();
-        $response = null;
+        try {
+            $url = "http://raw.githubusercontent.com/{$name[0]}/{$name[1]}/master/";
 
-        try{
-            $response = $client->get($url . $manager->file);
+            $client = new Client();
+
+            try{
+                $response = $client->get($url . $manager->file);
+
+                try{
+                    $json =  json_decode($response->getBody());
+                    print_r($json);
+                    $response = [
+                        "name" => $fields["name"],
+                        "manager_id" => $fields["manager"],
+                        "dependency_name" => $json->name
+                    ];
+                }
+                catch (\Exception $e)
+                {
+                    $response = [
+                        "error" => "There isn't a name field in the {$manager->file}",
+                        "code" => 404
+                    ];
+                }
+
+            }
+            catch (\ErrorException $e)
+            {
+                $response = [
+                    "error" => "The repository name is not valid",
+                    "code" => 400
+                ];
+            }
+            catch (RequestException $e)
+            {
+                $response = [
+                    "error" => "The repository does not exist",
+                    "code" => 404
+                ];
+            }
         }
         catch (\ErrorException $e)
         {
-            return ["error" => "The repository name is not valid"];
-        }
-        catch (RequestException $e)
-        {
-            return ["error" => "The repository does not exist"];
+            $response = [
+                "error" => "The repository name is not valid",
+                "code" => 400
+            ];
         }
 
-        try{
-            $json =  json_decode($response->getBody());
-            return [
-                "name" => $fields["name"],
-                "manager_id" => $fields["manager"],
-                "dependency_name" => $json->name
-            ];
-        }catch (\Exception $e) {
-            return ["error" => "There isn't a name field in the {$manager->file}"];
-        }
+        return $response;
     }
 } 
